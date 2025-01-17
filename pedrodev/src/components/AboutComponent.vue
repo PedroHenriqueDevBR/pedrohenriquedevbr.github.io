c464e12aad0a42c9694e496834782b4fc0a91ee1
PedroHenriqueDevBR <p.lyma18@gmail.com>
2025-01-17 00:34:49 -0300

    style: added prog lang images

--- pedrodev/src/components/AboutComponent.vue
+++ pedrodev/src/components/AboutComponent.vue
@@ -1,11 +1,13 @@
 <template>
-    <div class="description-container">
+    <div class="container">
         <h1 class="primary-color-text">Sobre</h1>
         <div class="description">
             <div class="vertical-divider primary-color"></div>
             <div>
-                <p>Sou desenvolvedor de software com experiência na área e apto para resolver problemas relacionados à
-                    tecnologias</p>
+                <p>
+                    Sou desenvolvedor de software com experiência na área e apto para resolver problemas relacionados à
+                    tecnologias
+                </p>
 
                 <p>Sou estudante de desenvolvimento de softwares e utilizo os conhecimentos adquiridos no meu dia a dia,
                     gosto muito de estudar e repassar os conhecimentos adquiridos para outras pessoas, seja por meio de
@@ -15,11 +17,54 @@
         </div>
     </div>
 
+    <div class="container mt-md text-center">
+        <h1 class="primary-color-text text-center">Skills</h1>
+        <div class="skills">
+            <div class="card">
+                <img src="@/assets/images/python.png" alt="Python">
+                <span>Python</span>
+            </div>
+
+            <div class="card">
+                <img src="@/assets/images/css.png" alt="css">
+                <span>CSS</span>
+            </div>
+            <div class="card">
+                <img src="@/assets/images/flutter.png" alt="flutter">
+                <span>Flutter</span>
+            </div>
+            <div class="card">
+                <img src="@/assets/images/git.png" alt="git">
+                <span>Git</span>
+            </div>
+            <div class="card">
+                <img src="@/assets/images/html.png" alt="html">
+                <span>HTML</span>
+            </div>
+            <div class="card">
+                <img src="@/assets/images/javascript.png" alt="javascript">
+                <span>JavaScript</span>
+            </div>
+            <div class="card">
+                <img src="@/assets/images/linux.png" alt="linux">
+                <span>Linux</span>
+            </div>
+            <div class="card">
+                <img src="@/assets/images/mysql.png" alt="mysql">
+                <span>MySQL</span>
+            </div>
+            <div class="card">
+                <img src="@/assets/images/postgresql.png" alt="postgresql">
+                <span>PostgreSQL</span>
+            </div>
+        </div>
+
+    </div>
 
 </template>
 
 <style scoped>
-.description-container {
+.container {
     margin: 0 auto;
     width: 1000px !important;
     display: flex;
@@ -28,9 +73,10 @@
     justify-content: center;
 }
 
-.description-container h1 {
+.container h1 {
     font-weight: 500;
     font-size: 3rem;
+    margin-bottom: 16px;
 }
 
 .description {
@@ -46,5 +92,52 @@
     border-radius: 16px;
     height: auto;
     margin: 0 16px;
+}
+
+.mt-md {
+    margin-top: 32px;
+}
+
+.container.text-center {
+    align-items: center;
+}
+
+.text-center {
+    text-align: center !important;
+}
+
+.skills {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+}
+
+.skills .card {
+    width: 175px;
+    height: 175px;
+    background-color: var(--color-background-soft);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 8px;
+    transition: 200ms ease-in-out;
+    margin: 16px;
+}
+
+.skills .card:hover {
+    background-color: var(--color-background-muted);
+}
+
+.skills .card img {
+    width: 100px;
+}
+
+.skills .card span {
+    margin-top: 16px;
+    font-weight: 600;
+    font-size: 1.2rem;
 }
 </style>
